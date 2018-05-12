d827c0ae442c14720b8341e2a82e8c6a18d8e014
Bret Pehrson <bretpehrson@Brets-iMac.local>
2018-05-12 14:45:59 -0600

    Initial Commit

---
+++ DemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using DemoMVC.Models;
+
+namespace DemoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //  site/home/index
+        public ActionResult Index()
+        {
+            var mvcName = typeof(Controller).Assembly.GetName();
+            var isMono = Type.GetType("Mono.Runtime") != null;
+
+            ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+            ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+
+            return View();
+        }
+
+        //  site/home/abouts
+        public ActionResult About()
+		{
+			ViewBag.Message = "This is the about page";
+
+			return View();
+		}
+
+		[HttpPost]
+        public ActionResult About(string userData)
+		{
+			ViewBag.Message = userData;
+			return View();
+		}
+
+        //  site/home/list
+        public ActionResult List()
+		{
+			return View(TechItem.GetItems());
+		}
+    }
+}
---
+++ DemoMVC/Models/TechItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoMVC.Models
+{
+    public class TechItem
+    {
+        public string Name
+		{
+			get;
+			set;
+		}
+
+		public TechItem()
+        {
+			
+        }
+
+        public static IEnumerable<TechItem> GetItems()
+		{
+			return new List<TechItem>
+			{
+				new TechItem {Name = "ASP"},
+				new TechItem {Name = "ASP.NET"},
+				new TechItem {Name = "ASP.NET Core"}
+			};
+		}
+    }
+}
